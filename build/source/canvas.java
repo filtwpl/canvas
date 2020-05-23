5ffcd5c14180a7a4e5bfa3d0e092025c9f5b371d
jacqs <filtwpl@gmail.com>
2020-05-23 15:11:01 -0700

    key red

--- build/source/canvas.java
+++ build/source/canvas.java
@@ -1,18 +1,34 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class canvas extends PApplet {
+
 
 public SideBar menu;
 public int currentR = 0;
 public int currentG = 0;
 public int currentB = 0;
 
-void setup() {
-  size(1300, 900);
+public void setup() {
+  
   background(255);
   menu = new SideBar();
   menu.addButton(255, 0, 0);
   menu.show();
 }
 
-void draw() {
+public void draw() {
 
 
   if (mousePressed && mouseX>50 && pmouseX>50) {
@@ -89,4 +105,14 @@ public class Button {
   public int getB() {return myB;}
   public boolean getOn() {return on;}
 
+}
+  public void settings() {  size(1300, 900); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "canvas" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
